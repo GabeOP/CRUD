e8e9511c121a60e0b018ed2c111429b42510b977
GabeOP <gabrieloliveirap1@hotmail.com>
2023-02-27 20:00:42 -0300

    Logo/Header responsividade

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 import '../../style/Header.css'
+import Logo from '../../public/logo.png'
 
 export default function Header() {
   return (
     <>
       <nav id='navHeader' className="bg-black navbar navbar-expand-md">
         <div className="container-fluid" id="container-navbar">
-
-          <Link to={'/planos'} className="text-info navbar-brand ">Shar'k Tech</Link>
+          <img id='imgLogo' src={Logo} alt="" />
+          <Link to={'/planos'} className="text-info navbar-brand " id='tituloHeader'>Shar'k Tech</Link>
           <button
             className="bg-info navbar-toggler"
             type="button"
@@ -20,16 +21,16 @@ export default function Header() {
             <span className="navbar-toggler-icon text-info"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+            <ul id='ulHeader' className="navbar-nav me-auto mb-2 mb-md-0">
               <li className="nav-item">
                 <Link className="text-info nav-link" to={'/planos'}>
                   Planos
                 </Link>
               </li>
               <li className="nav-item">
-                <a className="text-info nav-link" href="#Titulo1">
-                  Quem Somos
-                </a>
+                <Link to={'/assinatura'} className="text-info nav-link" href="#Titulo1">
+                  Meu plano
+                </Link>
               </li>
             </ul>
             <form className="d-flex gap-3">
@@ -38,12 +39,12 @@ export default function Header() {
                 type="submit"
                 id="cadastroI"
               >
-                <a className="text-light " href="cadastro.html">
+                <a className="text-light ">
                   Cadastre-se
                 </a>
               </button>
               <button className=" btn btn-outline-info">
-                <a className="text-light" href="login.html">
+                <a className="text-light">
                   Login
                 </a>
               </button>
